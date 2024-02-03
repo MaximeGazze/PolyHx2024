6fd696e847c1d2d840f3c0f4e7546b2ede4f45d3
MaximeGazze <maxime.gazze@videotron.ca>
2024-02-03 17:13:02 -0500

    ajout des components

---
+++ src/lib/components/card.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  export let imgSrc: string = '';
+  export let imgAlt: string = '';
+</script>
+
+<div>
+  <img src={imgSrc} alt={imgAlt} />
+  <p>
+    <slot />
+  </p>
+</div>
+
+<style>
+  div {
+    display: flex;
+    background-color: rgb(116, 99, 99);
+    align-items: center;
+    padding: 0 1rem;
+    gap: 1rem;
+    border-radius: 0.5rem;
+  }
+
+  img {
+    width: 3rem;
+    height: 3rem;
+    object-fit: cover;
+  }
+
+  p {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Card from '$lib/components/card.svelte';
+  import Cardlist from '$lib/components/cardlist.svelte';
+</script>
+
+<svelte:head>
+  <title>Mangeux de yop</title>
+</svelte:head>
+
+<Cardlist>
+  <Card
+    imgSrc="https://i0.wp.com/boingboing.net/wp-content/uploads/2024/01/image-114.png?fit=1348%2C968&ssl=1"
+    imgAlt="lol">
+    voici un mangeux de yop au banane aaaaaaaaaaaaaaaaa
+  </Card>
+  <Card
+    imgSrc="https://i0.wp.com/boingboing.net/wp-content/uploads/2024/01/image-114.png?fit=1348%2C968&ssl=1"
+    imgAlt="lol">
+    voici un autre mangeux de yop au banane aaaaaaaaaaaaaaaaa
+  </Card>
+</Cardlist>
+<button>es-tu un mangeux de yop?</button>
+
+<style>
+  button {
+    background-color: rgb(209, 84, 84);
+    padding: 0.5rem;
+    margin: 0.5rem 0;
+    min-width: 3rem;
+    border: none;
+    border-radius: 0.5rem;
+    cursor: pointer;
+  }
+</style>
